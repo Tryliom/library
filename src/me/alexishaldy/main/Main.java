d265161b6d298162cdc7c91a5d581dd95e468f5c
haldy <haldy@PC100531.hq.k.grp>
2017-05-03 15:18:03 +0200

    Commit 3

--- src/me/alexishaldy/main/Main.java
+++ src/me/alexishaldy/main/Main.java
@@ -1,8 +1,31 @@
 package me.alexishaldy.main;
 
+import me.alexishaldy.classes.Book;
+import me.alexishaldy.classes.Library;
+import me.alexishaldy.classes.User;
+
 public class Main {
+	private static int displayTot=0; 
+	public static void main(String[] args) {		
+		Library lib = Library.getLibrary();
+		lib.addBook(new Book("Hello world", "John", 1999));
+		lib.addBook(new Book("Hello worlds", "Johna", 1995));
+		display(lib.listBook());
+		lib.removeBook("Hello worlds");
+		display(lib.listBook());
+		lib.addBook(new Book("Hello worlds", "Johna", 1995));
+		lib.removeBook();
+		display(lib.listBook());
+		lib.addBook(new Book("April fools", "Marie", 1888));
+		lib.addBook(new Book("The Java", "Paul", 2015));
+		// Finir le reste avant de commencer le sortedBy
+		display(lib.listBook());
+		lib.addUser(new User("Alexis", "Haldy"));
+	}
 	
-	public static void main(String[] args) {
-		int nb;
+	
+	public static void display(String s) {
+		displayTot++;
+		System.out.println("Display "+displayTot+":\n"+s+"\n\n");
 	}
 }
