411be036ee34080d54832109344e6a8782d29005
Haldy Alexis <alexis.haldy@nagra.com>
2017-05-04 09:32:09 +0200

    test

--- src/me/alexishaldy/main/Main.java
+++ src/me/alexishaldy/main/Main.java
@@ -20,6 +20,8 @@ public class Main {
 		lib.addBook(new Book("Hello worlds", "Johna", 1995, lib.getUserByNameOrLastname("Paul")));
 		display(lib.listBook());
 		
+		
+		
 		lib.removeBook("Hello worlds");
 		display(lib.listBook());
 		
