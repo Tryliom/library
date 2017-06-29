67c9bf36bf921dbcf6cdba73130322ce1fd029e7
Alexis Haldy <alexis.haldy@nagra.com>
2017-06-29 14:46:18 +0200

    Admin panel 4

--- web/list_lib.php
+++ web/list_lib.php
@@ -3,12 +3,12 @@ if (strpos($_SERVER['PHP_SELF'], 'list_lib.php') !== false) {
     header("Location: index.php");
 }
 
-if (isset($_REQUEST['update'])) {
+if (isset($_REQUEST['lib_list']) && isset($_REQUEST['update'])) {
 	$name = $_REQUEST['name'];
 	$adress = $_REQUEST['adress'];
 	$id = $_REQUEST['id'];
 	$ch = curl_init();
-	echo "dsfsd";
+	
 	curl_setopt($ch, CURLOPT_URL,"http://localhost:6080/library/edit/$name/$adress/$id");
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -17,60 +17,99 @@ if (isset($_REQUEST['update'])) {
 	if ($s==="true") {
 		echo "<p style='color:#33ff33'>Données mises à jour !</p>";
 	} else {
-		echo "<p style='color:#ff3333'>Erreur</p>";
+		echo "<p style='color:#ff3333'>Erreur: $s ; ".("http://localhost:6080/library/edit/$name/$adress/$id")."</p>";
 	}
 }
 
+// WORK
 if (isset($_REQUEST['lib_list']) && isset($_REQUEST['delete'])) {
-	$bid = $_REQUEST['id'];
-	$uid = $id;
-	$lib = $_SESSION['lib'];
+	$id = $_REQUEST['id'];
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL,"http://localhost:6080/user/return/$bid/$uid/$lib");
+	curl_setopt($ch, CURLOPT_URL,"http://localhost:6080/library/delete/$id");
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$s = curl_exec ($ch);
 	curl_close ($ch);
 	if ($s==="true") {
-		echo "<p style='color:#33ff33'>Vous avez rendu le livre !</p>";
+		echo "<p style='color:#33ff33'>Librairie supprimée !</p>";
+	} else {
+		echo "<p style='color:#ff3333'>Erreur</p>";
+	}
+}
+
+if (isset($_REQUEST['lib_add']) && isset($_REQUEST['add'])) {
+	$name = $_REQUEST['name'];
+	$adress = $_REQUEST['adress'];
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL,"http://localhost:6080/library/add");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // N'affiche pas le résultat dans la page
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "name=$name&adress=$adress");
+	$s = curl_exec ($ch);
+	curl_close ($ch);
+	if ($s==="true") {
+		echo "<p style='color:#33ff33'>Librairie supprimée !</p>";
 	} else {
 		echo "<p style='color:#ff3333'>Erreur $s</p>";
 	}
 }
+
 $json_source = file_get_contents('http://localhost:6080/library/get/');
 $jd= json_decode($json_source);
-	$h = "<h1>Gestion des librairies</h1><table id='list' cellspacing='10'><th>ID</th><th>Nom</th><th>Adresse</th><th>Options</th><tr>";
+	$h = "<h1>Gestion des librairies</h1>
+	<table id='list' cellspacing='10'>
+		<tr>
+			<th>ID</th>
+			<th>Nom</th>
+			<th>Adresse</th>
+			<th>Options</th>
+		</tr>";
 	$m = "";
 	for ($i=0;$i<sizeof($jd);$i++) {
 		$lid = $jd[$i]->id;
 		$adress = $jd[$i]->adress;
 		$name = $jd[$i]->name;
 		$m .= "
-		<form name='lib_list' method='post'>
-		<td ".setWidth($name).">
-		<input id='text' type='text' value='$lid' name='id' disabled/>
-		</td>
-		<td ".setWidth($name).">
-		<input id='text' type='text' name='name' value='$name' />
-		</td><td ".setWidth($adress).">
-		<input id='text' type='text' name='adress' value='$adress' />
-		</td><td> <input style='width:100%;' id='button' type='submit' value='Sauvegarder' name='update' /> <input style='width:100%;' id='button' type='submit' value='Supprimer' name='delete' /> </td>
-		</form></tr>";
+			<tr>
+				<form method='post'>
+					<input type='hidden' name='lib_list' />
+					<td ".setWidth($lid).">
+						<input id='text' type='text' value='$lid' name='id' />
+					</td>
+					<td ".setWidth($name).">
+						<input id='text' type='text' name='name' value='$name' />
+					</td>
+					<td ".setWidth($adress).">
+						<input id='text' type='text' name='adress' value='$adress' />
+					</td>
+					<td> 
+						<input style='width:100%;' id='button' type='submit' value='Sauvegarder' name='update' />
+						<input style='width:100%;' id='button' type='submit' value='Supprimer' name='delete' /> 
+					</td>
+				</form>
+			</tr>";
 	}
-	$b = "<form name='lib_add' method=post><tr>
-	<td>
-	<input id='text' type='text' name='id' disabled/>
-	</td>
-	<td>
-	<input id='text' type='text' name='name' />
-	</td><td>
-	<input id='text' type='text' name='adress' />
-	</td>
-	<td><input style='width:100%;' id='button' type='submit' value='Ajouter une nouvelle librairie' name='add' /></td></tr></form></table>";
-	if ($m==="") 
-		echo "<p>Aucunes librairies</p>";
-	else
+	$b = "
+		<form method=post>	
+			<input type='hidden' name='lib_add' />
+			<tr>
+				<td>
+					<input id='text' type='text' name='id' disabled/>
+				</td>
+				<td>
+					<input id='text' type='text' name='name' />
+				</td>
+				<td>
+					<input id='text' type='text' name='adress' />
+				</td>
+				<td>
+					<input style='width:100%;' id='button' type='submit' value='Ajouter une nouvelle librairie' name='add' />
+				</td>	
+			</tr>
+		</form>
+	</table>";
 		echo "$h $m $b";
 	
 	
@@ -113,16 +152,10 @@ $jd= json_decode($json_source);
 	<td><input id='textmin' type='text' name='editor' /></td>
 	<td><input id='textmin' type='text' name='desc' /></td>
 	<td><input id='textmin' type='text' name='lib' /></td>
-	<td><input style='width:100%;' id='button' type='submit' value='Ajouter une nouvelle librairie' name='add' /></td></tr></form></table>";
-	if ($m==="") 
-		echo "<p>Aucun livres disponibles</p>";
-	else
+	<td><input style='width:100%;' id='button' type='submit' value='Ajouter un nouveau livre' name='add' /></td></tr></form></table>";
 		echo "$h $m $b";
 	
 	
-	
-	// Add new lib et new book, laisser des cases libres pour, le bouton complète juste l'action
-	// Mettre les infos dans des input de text pour modifier
 	function setWidth($s) {
 		return "style='width:".(strlen($s)+4)."%;'";
 	}
