16eaebaaf97bb31f1594a8621730ed6f1dc809c8
haldy <alexis.haldy@nagra.com>
2017-09-04 10:10:16 +0200

    Finition

--- src/me/alexishaldy/db/connection/DBConnectionAdapter.java
+++ src/me/alexishaldy/db/connection/DBConnectionAdapter.java
@@ -141,7 +141,7 @@ public class DBConnectionAdapter extends DBConnection {
 			
 			// Retrieve the results
 			while (resultSet.next()) {
-				for (int i = 0;resultSet.getString(i)!=null;i++) {
+				for (int i = 1;resultSet.getString(i)!=null;i++) {
 					result.addElement(resultSet.getString(i));
 				}
 			}
